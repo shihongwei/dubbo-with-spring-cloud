bebb06d3a86b00af44fe4ee47de4f4224c94ad19
wuzhuoyan <wuzhuoyan@live.com>
2017-09-07 10:26:44 +0800

    add api files

---
+++ dubbo-adv-soa/adv-api/src/main/java/sample/ds/cn/service/DubboSay.java
@@ -0,0 +1,12 @@
+package sample.ds.cn.service;
+
+import com.gac.core.resp.Response;
+
+/**
+ * Created by wu on 17/8/31.
+ */
+public interface DubboSay {
+
+    public String dubboSayHello(String word);
+
+}
